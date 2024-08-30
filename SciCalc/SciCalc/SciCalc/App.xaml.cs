767ef8a2ae2efc71570813e5e0294905556f625f
LAUREN\lauren <laurenpnn@gmail.com>
2024-08-30 13:01:52 +0300

    dd

--- SciCalc/SciCalc/SciCalc/App.xaml.cs
+++ SciCalc/SciCalc/SciCalc/App.xaml.cs
@@ -4,7 +4,7 @@ using Microsoft.UI.Windowing;
 using Windows.Graphics;
 #endif
 
-
+using SciCalc.Views;
 
 namespace SciCalc
 {
@@ -12,24 +12,25 @@ namespace SciCalc
     {
         const int WindowWidth = 540;
         const int WindowHeight = 1000;
+
         public App()
         {
             InitializeComponent();
 
 #if WINDOWS
- Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-    {
-    var mauiWindow = handler.VirtualView;
-    var nativeWindow = handler.PlatformView;
-    nativeWindow.Activate();
-    IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-    WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-    AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-    appWindow.Resize(new SizeInt32(WindowWidth,WindowHeight));
-    });
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth,WindowHeight));
+        });
 #endif
 
-            MainPage = new AppShell();
+            MainPage = new CalculatorPage();
         }
     }
 }
